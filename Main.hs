285f772c7c2d1c4db4c90e5aa2e1a515c984f883
Mitchell Rosen <mitchellwrosen@gmail.com>
2024-02-24 20:54:01 -0500

    add pg cluster create

--- Main.hs
+++ Main.hs
@@ -1,10 +1,14 @@
 module Main where
 
+import Control.Applicative (asum)
 import Control.Exception (bracket)
+import Data.Foldable (fold)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO.Utf8 qualified as Text
-import System.Exit (ExitCode)
+import Options.Applicative qualified as Opt
+import System.Directory qualified as Directory
+import System.Exit (ExitCode (..), exitWith)
 import System.IO qualified as IO
 import System.Posix.IO.ByteString qualified as Posix
 import System.Process qualified as Process
@@ -12,12 +16,66 @@ import Prelude hiding (lines)
 
 main :: IO ()
 main = do
-  -- TODO check all required executables
+  commandLineInterface
+    [ Opt.showHelpOnEmpty,
+      Opt.showHelpOnError
+    ]
+    [ Opt.progDesc "Postgres utility knife."
+    ]
+    [ subcommands
+        [ subcommand
+            []
+            "cluster"
+            ( subcommands
+                [ subcommand [] "create" $
+                    pgClusterCreate
+                      <$> textArg [Opt.metavar "«name»"]
+                ]
+            )
+        ]
+    ]
 
-  result <- process "ls" []
-  print result
+pgClusterCreate :: Text -> IO ()
+pgClusterCreate name = do
+  Directory.createDirectoryIfMissing False "_postgres_clusters"
+  (out, err, code) <-
+    process
+      "initdb"
+      [ "--pgdata=_postgres_clusters/" <> name,
+        "--encoding=UTF8"
+      ]
+  case code of
+    ExitSuccess -> pure ()
+    ExitFailure _ -> do
+      Text.putStr (Text.unlines out)
+      Text.putStr (Text.unlines err)
+      exitWith code
 
-  pure ()
+------------------------------------------------------------------------------------------------------------------------
+-- Cli utils
+
+commandLineInterface :: [Opt.PrefsMod] -> [Opt.InfoMod (IO a)] -> [Opt.Parser (IO a)] -> IO a
+commandLineInterface prefs info parsers = do
+  action <-
+    Opt.customExecParser
+      (Opt.prefs (fold prefs))
+      (Opt.info (asum parsers) (fold info))
+  action
+
+subcommands :: [Opt.Mod Opt.CommandFields a] -> Opt.Parser a
+subcommands commands =
+  Opt.hsubparser (Opt.metavar "«command»" <> fold commands)
+
+subcommand :: [Opt.InfoMod a] -> String -> Opt.Parser a -> Opt.Mod Opt.CommandFields a
+subcommand info name parser =
+  Opt.command name (Opt.info parser (fold info))
+
+textArg :: [Opt.Mod Opt.ArgumentFields Text] -> Opt.Parser Text
+textArg opts =
+  Opt.strArgument (fold opts)
+
+------------------------------------------------------------------------------------------------------------------------
+-- Subprocess utils
 
 process :: Text -> [Text] -> IO ([Text], [Text], ExitCode)
 process name args = do
@@ -46,7 +104,7 @@ process name args = do
             Process.cwd = Nothing,
             Process.delegate_ctlc = False,
             Process.detach_console = False,
-            Process.env = Just [],
+            Process.env = Nothing,
             Process.new_session = False,
             Process.std_err = Process.UseHandle stderrWriteHandle,
             Process.std_in = Process.NoStream,
